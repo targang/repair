3d6f1ee68c458d00ad7841be09820a7d82c9e145
Bulat Safin <bsafin@outlook.com>
2021-05-05 17:25:24 +0300

    Email notifications add

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
 from django.http.request import HttpRequest
 from django.views.decorators.http import require_http_methods
-from pages.forms import OrderForm
+from django.core.mail import send_mail
+
 
 from .models import Order
 
@@ -11,9 +12,21 @@ def create_order(request: HttpRequest):
     name = request.POST["name"]
     phone = request.POST["phone"]
     message = request.POST["message"] or None
+    email = f"Имя: {name}\nТелефон: {phone}\n"
+    if message:
+        email += f'Сообщение: {message}'
     if name and phone:
-        Order.objects.create(name=name, phone=phone, message=message)
-
-        return redirect(
-            "/?msg=true"
-        )
+        try:
+            Order.objects.create(name=name, phone=phone, message=message)
+            send_mail(
+                subject="Новая заявка",
+                message=email,
+                from_email='email',
+                recipient_list=[
+                    "email",
+                ],
+                fail_silently=False,
+            )
+            return redirect("/?msg=true")
+        except:
+            return redirect("/")
--- repair/settings.py
+++ repair/settings.py
@@ -99,3 +99,10 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'key'
+EMAIL_USE_TLS = True
